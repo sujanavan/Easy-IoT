28a6a79837b2810dd66df79093eeb933a595a4d1
Sujanavan Tiruvayipati <sujanavan_cse@mvsrec.edu.in>
2019-09-27 16:20:46 +0000

    Add new file

---
+++ Cloud Server/basic/index.php
@@ -0,0 +1,10 @@
+
+<form 	action="sense.php"     method="get">
+<input type=hidden name=data value=1>
+  <button> O N </button>
+</form>
+
+<form 	action="sense.php"     method="get">
+<input type=hidden name=data value=0>
+  <button> O F F </button>
+</form>
