d26f8f21522637368446ae49d5081cd01b46fb25
Sujanavan Tiruvayipati <49283545+sujanavan@users.noreply.github.com>
2022-09-26 02:21:39 +0530

    Update actuator.ino

--- Arduino IDE (NodeMCU)/Internet/actuator.ino
+++ Arduino IDE (NodeMCU)/Internet/actuator.ino
@@ -8,13 +8,6 @@ ESP8266WiFiMulti WiFiMulti;
 
 void setup() {
   Serial.begin(115200);
-
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
-
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP("<<<SSID>>>", "<<<PASSWORD>>>");
 }
