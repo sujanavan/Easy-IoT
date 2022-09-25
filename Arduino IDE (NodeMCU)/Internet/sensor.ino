a4a346a04dc13196c34d84cae658181422ca6b68
Sujanavan Tiruvayipati <49283545+sujanavan@users.noreply.github.com>
2022-09-25 23:19:12 +0530

    Update sensor.ino

--- Arduino IDE (NodeMCU)/Internet/sensor.ino
+++ Arduino IDE (NodeMCU)/Internet/sensor.ino
@@ -1,24 +1,75 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 
-const char* ssid = "YOUR WIFI NAME";  // YOUR WIFI NAME
-const char* password = "YOUR WIFI PASSWORD";  // YOUR WIFI PASSWORD
+ESP8266WiFiMulti WiFiMulti;
 
-void setup () {
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+String URL="<<<HTTP-URL-ONLY-HTTP>>>/write.php?data=";
+int temp=0;
+
+void setup() {
+
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
     delay(1000);
-    Serial.println("Connecting WiFi...");
-    }
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("<<<WIFI-NAME>>>", "<<<WIFI-PASSWORD>>>");
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); //Initially LED is OFF - Built-in LED works in REVERSE - NOT Gate
+
 }
- 
+
 void loop() {
-  float sensor = (analogRead(A0)/1024.0)*330;         // LM35 temperature sensor conversions
-  //Check for change in temperature before upload to avoid redundant data
-  String url="URL/sense.php?data="+String(sensor);  
-  HTTPClient http;
-  http.begin(url);
-  int httpCode = http.GET();
-  http.end();
-  delay(15000);     //Based on Cloud provider statistics
+
+  /* 
+  LM-35 SENSOR INITIALIZATION TO BE ADDED IN SETUP
+  THEN HERE GET SENSOR VALUE
+  CONVERT THE VALUE into temp variable  
+  */  
+  
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+    WiFiClient client;
+    HTTPClient http;
+    
+    Serial.println(URL+temp);
+
+    Serial.print("[HTTP] begin...\n");
+    if (http.begin(client, URL+temp)) {  // HTTP URL added to temp
+
+
+      Serial.print("[HTTP] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = http.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          Serial.printf("[HTTP] Data Sent to Server Successfully\n");
+        }
+      } else {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+
+      http.end();
+    } else {
+      Serial.printf("[HTTP} Unable to connect\n");
+    }
+  }
+
+  delay(2000);
 }
