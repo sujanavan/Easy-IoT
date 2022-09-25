3e2be3e9f05b9eb32c77750519d1fbef2c97088b
Sujanavan Tiruvayipati <49283545+sujanavan@users.noreply.github.com>
2022-09-26 03:13:36 +0530

    Update write.php

--- Cloud Server/2 - date chart/write.php
+++ Cloud Server/2 - date chart/write.php
@@ -1,13 +1,12 @@
 <?php
 //check if request is empty
-if(empty($_GET["l"]) || empty($_GET["t"]))
-die();
+if(empty($_GET["data"])) die();
 //set date and time to IST
 date_default_timezone_set('Asia/Kolkata');
 //capture current data time in specific format Year,month,date Hours,Min,Sec
 $dt = date('Y-m-d H:i:s');
 $myfile = fopen("data", "a") or die("Unable to open file!");
-$txt = "{$_GET["l"]},{$dt},{$_GET["t"]}\n";
+$txt = "{$dt},{$_GET["data"]}\n";
 fwrite($myfile, $txt);
 fclose($myfile);
 ?>
