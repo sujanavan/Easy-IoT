aa467bf3ad03b0b7392001caf9bc896855248c2d
Sujanavan Tiruvayipati <49283545+sujanavan@users.noreply.github.com>
2022-09-26 02:52:29 +0530

    Update index.php

--- Cloud Server/basic/index.php
+++ Cloud Server/basic/index.php
@@ -1,10 +1,5 @@
 
-<form 	action="sense.php"     method="get">
-<input type=hidden name=data value=1>
-  <button> O N </button>
-</form>
-
-<form 	action="sense.php"     method="get">
-<input type=hidden name=data value=0>
-  <button> O F F </button>
+<form 	action="write.php"     method="get">
+<input type=text name=data>
+<input type=submit>
 </form>
