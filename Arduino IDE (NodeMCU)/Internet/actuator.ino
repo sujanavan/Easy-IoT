a91e8e8987a1da08f8dca719901d45712e8061c0
Sujanavan Tiruvayipati <49283545+sujanavan@users.noreply.github.com>
2022-09-25 22:42:36 +0530

    Update actuator.ino

--- Arduino IDE (NodeMCU)/Internet/actuator.ino
+++ Arduino IDE (NodeMCU)/Internet/actuator.ino
@@ -1,34 +1,54 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 
-const char* ssid = "YOUR WIFI NAME";  // YOUR WIFI NAME
-const char* password = "YOUR WIFI PASSWORD";  // YOUR WIFI PASSWORD
+ESP8266WiFiMulti WiFiMulti;
 
-int data=0;
+void setup() {
+  Serial.begin(115200);
 
-void setup () {
-  pinMode(LED_BUILTIN,OUTPUT);
-  Serial.begin(9600);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
     delay(1000);
-    Serial.println("Connecting WiFi...");
-    }
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("<<<SSID>>>", "<<<PASSWORD>>>");
 }
- 
+
 void loop() {
-  String url="URL/data";  
-  HTTPClient http;
-  http.begin(url);
-  int httpCode = http.GET();
-  if (httpCode > 0) {
-    data = http.getString().toInt();
-    Serial.println(" Read = " + String(data));
-    if(data==0)
-    digitalWrite(LED_BUILTIN, LOW); 
-    if(data==1)
-    digitalWrite(LED_BUILTIN, HIGH);
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+    WiFiClient client;
+    HTTPClient http;
+    Serial.print("[HTTP] begin...\n");
+    if (http.begin(client, "<<<HTTP-URL-ONLY-HTTP>>>")) {  // HTTP
+
+      Serial.print("[HTTP] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = http.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = http.getString();
+          Serial.println(payload);
+        }
+      } else {
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+
+      http.end();
+    } else {
+      Serial.printf("[HTTP} Unable to connect\n");
     }
-  http.end();
-  delay(15000);         //Based on Cloud provider statistics
+  }
+  delay(5000);
 }
