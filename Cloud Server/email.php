fd121f7f45446bd3b0c8a30b78d2f46200c3f26b
Sujanavan Tiruvayipati <49283545+sujanavan@users.noreply.github.com>
2022-10-02 08:44:02 +0530

    Update email.php

--- Cloud Server/email.php
+++ Cloud Server/email.php
@@ -1,5 +1,5 @@
 <?php
-$to = "abcd@mail.com";
+$to = "abcd@mail.com"; //kindly add a correct mail id
 $subject = "Heat Alert";
 $message = "Temperature = Value C";
 $headers = 'From: IoT Server<donotreply>'; //no space between ..r<.. 
